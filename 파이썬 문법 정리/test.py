bab365272fc301cb4e8961bdc8b018b2b6974648
Hyeonjiwon <zlone1208@gmail.com>
2021-11-15 23:05:54 +0900

    modified test.py

---
+++ 파이썬 문법 정리/test.py
@@ -0,0 +1,21 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N):
+    # write your code in Python 3.6
+    arr = list(str(N))
+    print(arr.sort())
+    print("!!")
+    pass
+
+
+def main():
+    n = 213
+    print(solution(n))
+
+if __name__ == '__mai__':
+    main()
+    
