f85a0b4b9c930b7525f3e0a1bf935afbd6299a6c
Hyeonjiwon <zlone1208@gmail.com>
2021-11-15 23:11:46 +0900

    modified test.py

--- 파이썬 문법 정리/test.py
+++ 파이썬 문법 정리/test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
@@ -19,3 +20,30 @@ def main():
 if __name__ == '__mai__':
     main()
     
+=======
+import time
+from typing import AnyStr
+start = time.time()  # 시작 시간 저장
+
+num, base = 444, 5
+
+num = list(str(num))
+num = list(map(int, num))
+
+# print(num[::-1])
+answer = 0
+for i in enumerate(num):
+    
+    print(i)
+
+"""
+answer = 0
+for idx, number in enumerate(num[::-1]):
+    print(idx, number)
+    answer += int(number) * (base ** idx)
+"""
+print(answer)
+# print(int(str(num), base))
+
+print(f"{time.time() - start :.5f}")  # 실행 시간
+>>>>>>> a3fbbdafcfa09577fbd5412cf32dff80f228dc30
